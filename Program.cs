41f2e864ab88d606b4fc756f44cc0893bef6f0f4
Y.Arimitsu <yarimit@gmail.com>
2022-06-01 23:02:58 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelInterOpNetCore31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Excel.Application excel;
+            Excel.Workbook workbook;
+            Excel.Worksheet sheet;
+
+            try
+            {
+                // Start Excel and get Application object.
+                excel = new Excel.Application
+                {
+                    Visible = true
+                };
+
+                // Get a new workbook.
+                workbook = excel.Workbooks.Add(Missing.Value);
+                sheet = (Excel.Worksheet)workbook.ActiveSheet;
+
+                // Add table headers going cell by cell.
+                sheet.Cells[1, 1] = "First Name";
+                sheet.Cells[1, 2] = "Last Name";
+                sheet.Cells[1, 3] = "Full Name";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message} Line: {e.Source}");
+            }
+        }
+    }
+}
